1bdfb46c8feaab09b52011cf90cfafafdd6c603b
Diego Andres Perez Ruiz <dapr12@hotmail.com>
2014-06-04 18:10:18 +0100

    Combination Function

---
+++ Combination.R
@@ -0,0 +1,15 @@
+combinat<-function(n,p)
+  {
+    if (n<p){
+    
+        combinat <- 0
+            
+            }
+    
+    else 
+        {
+        
+            combinat<- exp(lfactorial(n)-(lfactorial(p)+lfactorial(n-p)))}
+            
+            
+}
