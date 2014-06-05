76051ab473524fc700d5a1ac4793a924f2870944
Diego Andres Perez Ruiz <dapr12@hotmail.com>
2014-06-05 17:29:04 +0100

    Create smoothmat.R

---
+++ smoothmat.R
@@ -0,0 +1,32 @@
+
+smoothmat<- function( fdata, rangeval ) 
+{
+  
+    fdat<- as.matrix(fdata)  
+    
+    m<-dim(fdat)[1] #Row(Curves)
+    
+    n<- dim(fdat)[2] #Col
+    
+    argvalmax<-rangeval[2]
+    
+    argvalmin<-rangeval[1]
+        
+    range<- round(seq(argvalmin, argvalmax, length.out = n), 3)
+    
+    smoothmat<-matrix(0,m,401)
+    
+    for( i in 1:m)
+    { 
+        for( j in 1:401  )
+        {
+        
+      h<-dpill(range, fdat[i,])   
+      smoothmat[i,j]<- locpoly(range, fdat[i,] , bandwidth = h)$y[j]
+        
+      }
+    }
+      
+    list(smoothdata = smoothmat, rangeval= range )
+    
+}
