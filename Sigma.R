0035fa5ae2cebef3edce2bd89a661a8bffdb8fd1
Diego Andres Perez Ruiz <dapr12@hotmail.com>
2014-06-05 13:20:00 +0100

    Update Sigma.R

--- Sigma.R
+++ Sigma.R
@@ -1,12 +1,15 @@
 
-Sigma<-function( x,y, l=1 ){
+Sigma<-function( x, y ){
   
   Sigma<-matrix(rep(0,length(x)*length(y)),nrow=length(x))
   
   for(i in 1:nrow(Sigma)){
     
-    for (j in 1:ncol(Sigma)) Sigma[i,j]<-exp(-1/2*(abs(x[i]-y[j])/l)^2)
-    
+    for (j in 1:ncol(Sigma)){ 
+      
+        Sigma[i,j]<-exp(-1/2*(abs(x[i]-y[j]))^2)
+    }
+ 
   }
   
   return(Sigma)
